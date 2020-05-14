b52d772e07517f4713e637aba7c7145ce05d7144
PR291961 <priya.mamuni.6060@gmail.com>
2020-05-14 18:13:03 +0530

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ';
+  name = 'Please click on a menu item';
 }
